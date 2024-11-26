305c4da91f4bbf1f250079da43e2784c8aa06b3b
1rayanahmed <rayanasif2004@gmail.com>
2024-11-26 16:28:28 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+var cacheName = 'petstore-v1';
+var cacheFiles = [
+    'index.html',
+    'products.js',
+    'petstore.webmanifest',
+    'images/cat.jpg',
+    'images/yarn.jpg',
+    'images/toy.jpg',
+    'images/toys.jpg',
+    'images/toys1.jpg',
+    'images/icon-store-512.png'
+    
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            return r || fetch(e.request).then(function (response) {
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
